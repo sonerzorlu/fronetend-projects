d88a8af26f4fec682e94c8240df584935f644a87
sonerzorlu <sonerzorlu@gmail.com>
2023-06-27 15:28:46 +0300

    css and html files ready

---
+++ DOM_PROJECT_modal/script.js
@@ -0,0 +1,11 @@
+
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector.('close-modal')
+const btnOpenModal = document.querySelector.('open-modal')
+console.log(btnOpenModal);
+
+for (let i = 0; i<btnOpenModal.length; i++)
+console.log(btnOpenModal[i].textContent);
+          m        m matchMedia.
