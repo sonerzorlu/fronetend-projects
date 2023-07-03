90542a8127c6adee6c0330da427095530d48709e
soner zorlu <sonerzorlu@gmail.com>
2023-07-03 12:24:54 +0300

    modal with dom done

--- DOM_PROJECT_modal/script.js
+++ DOM_PROJECT_modal/script.js
@@ -2,10 +2,18 @@
 
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
-const btnCloseModal = document.querySelector.('close-modal')
-const btnOpenModal = document.querySelector.('open-modal')
-console.log(btnOpenModal);
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+const closeModel = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
 
-for (let i = 0; i<btnOpenModal.length; i++)
-console.log(btnOpenModal[i].textContent);
-          m        m matchMedia.
+for (let i = 0; i<btnsOpenModal.length; i++)
+
+btnsOpenModal[i].addEventListener('click',function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+})
+btnCloseModal.addEventListener('click',closeModel)
+overlay.addEventListener('click',closeModel)
